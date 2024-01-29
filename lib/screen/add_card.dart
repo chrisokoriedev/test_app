7d434fedd0dee2ab39187be146d8eb7f36de9656
christian-iuil <okoriec01@gmail.com>
2024-01-29 14:58:09 +0100

    update

---
+++ lib/screen/add_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddCardScreen extends StatelessWidget {
+  const AddCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/screen/fund_wallet.dart
+++ lib/screen/fund_wallet.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:test_app/core/color.dart';
 import 'package:test_app/core/const.dart';
-
+import 'package:test_app/screen/add_card.dart';
+import 'package:test_app/screen/wallet_screen.dart';
+import 'package:test_app/widget/major_button.dart';
+import 'package:test_app/widget/texts.dart';
 
 class FundWalletScreen extends StatelessWidget {
   const FundWalletScreen({super.key});
@@ -18,6 +23,24 @@ class FundWalletScreen extends StatelessWidget {
         padding: appPadding,
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Gap(sizeMedia.height * 0.03),
+          InkResponse(
+            onTap: () => Get.to(const AddCardScreen()),
+            child: Row(
+              children: [
+                Icon(Icons.add, size: 18, color: AppColor.kPrimaryColor),
+                TextOf(
+                  'Add Card',
+                  16,
+                  AppColor.kPrimaryColor,
+                  fontWeight: FontWeight.w600,
+                )
+              ],
+            ),
+          ),
+          const Spacer(),
+          MajorButton('Fund', double.maxFinite,
+              press: () => Get.offAll(const WalletScreen())),
+          const Gap(20),
         ]),
       ),
     );
