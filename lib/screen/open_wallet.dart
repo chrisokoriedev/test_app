6e089c2056a722abf85b8b7ae08851584c941f62
christian-iuil <okoriec01@gmail.com>
2024-01-29 11:08:13 +0100

    adding image and open wallet screen

---
+++ lib/screen/open_wallet.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:test_app/core/const.dart';
+import 'package:test_app/widget/input_field.dart';
+import 'package:test_app/widget/major_button.dart';
+
+class OpenWalletScreen extends StatelessWidget {
+  const OpenWalletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wallet'),
+      ),
+      body: Padding(
+        padding: appPadding,
+        child: Column(
+          children: [
+            Gap(MediaQuery.of(context).size.height * 0.1),
+            InputField('Name', 'Auto-filled'),
+            const Gap(20),
+            InputField('Wallet ID', 'Auto-generated'),
+            const Spacer(),
+            MajorButton('Create Wallet', double.maxFinite, press: () {}),
+            const Gap(20),
+          ],
+        ),
+      ),
+    );
+  }
+}
